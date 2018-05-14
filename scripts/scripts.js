9f6082d15edf0383f6752554eefd8c0fb20f46aa
cvonwilczur <cvonwilczur@gmail.com>
2018-05-14 14:27:01 -0700

    Refactored code to have initialize function for future use

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,16 +3,21 @@
 flippedCard = document.querySelectorAll(".card-flipped");
 unflippedCard = document.querySelectorAll(".card");
 
+//Initialize
+init();
+
+//Initialize game-state function
+function init() {
+  addEventListenerCards();
+}
+// Flip functionality
+function hide() {
+  event.target.classList.toggle('visible')
+};
+
 // Adding Listener to all Cards for Flip functionality
-function addEventListenerCards(){
-  for (var i = 0; i < unflippedCard.length; i++){
+function addEventListenerCards() {
+  for (var i = 0; i < unflippedCard.length; i++) {
     unflippedCard[i].addEventListener('click', hide);
   }
 }
-
-addEventListenerCards();
-
-// Flip functionality
-function hide(){
-  event.target.classList.toggle('visible')
-  };
